9b3697c8e48fa649db149ed0dcd51cc805fa20cc
Zachary Sain <zachsain@gmail.com>
2023-12-01 14:37:27 -0600

    w

--- backend/app.js
+++ backend/app.js
@@ -1,57 +1,175 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-const app = express();
-const PORT = process.env.PORT || 1337;
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const ngrok = require('@ngrok/ngrok'); // Import ngrok
+// const app = express();
+// const PORT = process.env.PORT || 1337;
 
-app.use(bodyParser.json());
+// Use ngrok to connect to your local server
+// (async function () {
+//   const url = await ngrok.connect({
+//     addr: PORT,
+//     authtoken_from_env: true,
+//     verify_webhook_provider: "whatsapp",
+//     // Add other ngrok configuration options if needed
+//   });
+
+//   console.log(`Webhook server is publicly accessible at: ${url}`);
+// })();
+
+// app.use(bodyParser.json());
+
+// // Accepts POST requests at /webhook endpoint
+// app.post('/webhook', (req, res) => {
+//   // Parse the request body from the POST
+//   let body = req.body;
+
+//   // Check the incoming webhook message
+//   console.log(JSON.stringify(req.body, null, 2));
+
+//   // Handle WhatsApp webhook events
+//   if (req.body.object) {
+//     // Extract necessary information from the webhook payload
+//     // Your logic for handling WhatsApp webhook events goes here
+//     // ...
+
+//     // Respond with a 200 OK status
+//     res.sendStatus(200);
+//   } else {
+//     // Return a '404 Not Found' if the event is not from a WhatsApp API
+//     res.sendStatus(404);
+//   }
+// });
+
+// // Accepts GET requests at the /webhook endpoint for initial v
+// app.get('/webhook', (req, res) => {
+//   const verify_token = process.env.VERIFY_TOKEN;
+
+//   // Parse params from the webhook verification request
+//   let mode = req.query['hub.mode'];
+//   let token = req.query['hub.verify_token'];
+//   let challenge = req.query['hub.challenge'];
+
+//   // Check if a token and mode were sent
+//   if (mode && token) {
+//     // Check the mode and token sent are correct
+//     if (mode === 'subscribe' && token === verify_token) {
+//       // Respond with 200 OK and challenge token from the request
+//       console.log('WEBHOOK_VERIFIED');
+//       res.status(200).send(challenge);
+//     } else {
+//       // Respond with '403 Forbidden' if verify tokens do not match
+//       res.sendStatus(403);
+//     }
+//   }
+// });
+
+// // Listen on the specified port
+// app.listen(PORT, () => {
+//   console.log(`Webhook server is listening on port ${PORT}`);
+// });
+
+
+
+
+"use strict";
+
+const ngrok = require('@ngrok/ngrok');
+const PORT = 1337;
+
+
+(async function () {
+  const listener = await ngrok.connect({
+      addr: 8080,
+      authtoken_from_env: true,
+  });
+
+  console.log(`Ingress established at: ${listener.url()}`);
+})();
+
+
+// Access token for your app
+// (copy token from DevX getting started page
+// and save it as environment variable into the .env file)
+const token = process.env.WHATSAPP_TOKEN;
+
+// Imports dependencies and set up http server
+// const request = require("request"),
+const axios = require("axios").default,
+  express = require("express"),
+  body_parser = require("body-parser"),
+  // axios = require("axios").default,
+  app = express().use(body_parser.json()); // creates express http server
+
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log("webhook is listening"));
 
 // Accepts POST requests at /webhook endpoint
-app.post('/webhook', (req, res) => {
+app.post("/webhook", (req, res) => {
   // Parse the request body from the POST
   let body = req.body;
 
-  // Check the incoming webhook message
+  // Check the Incoming webhook message
   console.log(JSON.stringify(req.body, null, 2));
 
-  // Handle WhatsApp webhook events
+  // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
   if (req.body.object) {
-    // Extract necessary information from the webhook payload
-    // Your logic for handling WhatsApp webhook events goes here
-    // ...
-
-    // Respond with a 200 OK status
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
+      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      axios({
+        method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+        url:
+          "https://graph.facebook.com/v12.0/" +
+          phone_number_id +
+          "/messages?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "Ack: " + msg_body },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+    }
     res.sendStatus(200);
   } else {
-    // Return a '404 Not Found' if the event is not from a WhatsApp API
+    // Return a '404 Not Found' if event is not from a WhatsApp API
     res.sendStatus(404);
   }
 });
 
-// Accepts GET requests at the /webhook endpoint for initial v
-app.get('/webhook', (req, res) => {
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
+app.get("/webhook", (req, res) => {
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+   **/
   const verify_token = process.env.VERIFY_TOKEN;
 
   // Parse params from the webhook verification request
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
   // Check if a token and mode were sent
   if (mode && token) {
     // Check the mode and token sent are correct
-    if (mode === 'subscribe' && token === verify_token) {
+    if (mode === "subscribe" && token === verify_token) {
       // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
+      console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
-      // Respond with '403 Forbidden' if verify tokens do not match
+      // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
   }
-});
-
-// Listen on the specified port
-app.listen(PORT, () => {
-  console.log(`Webhook server is listening on port ${PORT}`);
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,201 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+import io from 'socket.io-client';
+
+
+// const socket = io('http://localhost:3000');
+const socket = io('http://localhost:3000', { transports: ['websocket'] });
 
 function App() {
+  const [logs, setLogs] = useState([])
+  const [allMessages, setAllMessages] = useState([])
+  const [messageContent, setMessageContent] = useState("")
+  const [conversationId, setConversationId] = useState("")
+  const [delivered, setDelivered] = useState(false)
+  const chatBoxRef = useRef(null);
+  const [latestDeliveredTimestamp, setLatestDeliveredTimestamp] = useState(null);
+  const sendMessage = (message) => {
+    // Send the message to the server
+    socket.emit('sendMessage', message);
+  };
+
+  useEffect(() => {
+    console.log('Effect is running');
+    sendMessage("hello");
+
+    const handleNewMessage = (newMessage) => {
+      console.log('New message received:', newMessage);
+      setLogs((prevLogs) => [...prevLogs, newMessage]);
+    };
+  
+    // Add the listener
+    socket.on('newMessage', handleNewMessage);
+  
+    // Cleanup the listener when the component is unmounted
+    return () => {
+      console.log('Cleaning up');
+      socket.off('newMessage', handleNewMessage);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('Logs changed:', logs);
+
+  }, [logs]);
+
+
+  useEffect(() => {
+    if (logs) {
+      // Create a set to store unique messages
+      const uniqueMessages = new Set(allMessages.map(msg => `${msg.sender}-${msg.text}-${msg.timestamp}`));
+  
+      logs.forEach((log) => {
+        if (log.body && log.body.entry) {
+          log.body.entry.forEach((entry) => {
+            if (entry.changes) {
+              entry.changes.forEach((change) => {
+                // Check if the field is 'messages' and it has the expected structure
+                if (change.field === 'messages' && change.value && change.value.messages) {
+                  change.value.messages.forEach((message) => {
+                    if (message.text && message.text.body) {
+                      const messageKey = `${message.from}-${message.text.body}-${message.timestamp}`;
+                      setConversationId(message.id)
+                      // Check if the message is unique
+                      if (!uniqueMessages.has(messageKey)) {
+                        const newMessage = {
+                          sender: message.from,
+                          text: message.text.body,
+                          timestamp: message.timestamp,
+                        };
+  
+                        // Add the unique message to the set
+                        uniqueMessages.add(messageKey);
+  
+                        // Update state with the new message
+                        setAllMessages(Array.from(uniqueMessages).map(key => {
+                          const [sender, text, timestamp] = key.split('-');
+                          return { sender, text, timestamp };
+                        }));
+                      }
+                    }
+                  });
+                }
+  
+                // Handle delivered status updates for the second type of log
+                if (change.field === 'messages' && change.value && change.value.statuses) {
+                  change.value.statuses.forEach((status) => {
+                    if (status.status === 'delivered') {
+                     
+                      setLatestDeliveredTimestamp((prevTimestamp) => {
+                        // Update timestamp only if it's newer
+                        if (!prevTimestamp || status.timestamp > prevTimestamp) {
+                         
+                          return status.timestamp;
+                        }
+                        return prevTimestamp;
+                      });
+                    }
+                 
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  }, [logs, allMessages]);
+  
+
+  
+console.log(allMessages)
+console.log(latestDeliveredTimestamp)
+console.log(delivered)
+
+function handleSubmit(e) {
+  e.preventDefault();
+    // Replicate the POST request
+    const url = 'https://graph.facebook.com/v18.0/174323312431372/messages';
+    const accessToken = 'EAAWJaxsIRAwBO9YSpLhNQECHwF6KBZCWrpGZAomudZAtYoJGEfLBSotxEavN4BtU8NBB10b3getyQXqSadkCVYt6D434SZASCkW2jpKNAo4i6ChfOuahLXZCOJzjgRYYQA1JVjOvqZAgcPY3CKFzILLwHbZBRtfk2VpZBqoPceUxfUDeX1TmNGIdkK6eVgXuiI4mVn4ZAic113gzfjFuyPgZDZD'; // Replace with your actual access token
+
+    const requestBody = {
+      messaging_product: 'whatsapp',
+      recipient_type: 'individual',
+      to: '19013052426',
+      type: 'text',
+      text: {
+        preview_url: false,
+        body: messageContent, // Use the messageContent from the state
+      },
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Message sent successfully:', data);
+        // Handle success, if needed
+      })
+      .catch((error) => {
+        console.error('Error sending message:', error);
+        // Handle error, if needed
+      });
+      
+      if (allMessages.length === 0 || latestDeliveredTimestamp > allMessages[allMessages.length - 1].timestamp) {
+        const newMessage = {
+          sender: '15550815927',
+          text: messageContent,
+          timestamp: latestDeliveredTimestamp,
+        };
+    
+        // Update state with the new message
+        setAllMessages((prevMessages) => [...prevMessages, newMessage]);
+    
+        // Clear the messageContent or update other relevant state values if needed
+        setMessageContent('');
+    
+    }
+  }
+
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        hello world it's time to party
-      </header>
+    <div className="chatScreen" ref={chatBoxRef}>
+        <div className="chatScreen--chatContainer">
+        <p className="chatScreen__timestamp"></p>
+      
+        { allMessages && allMessages.map((m) => (
+            m.sender=== '19013052426' ? (  
+                <div className="chatScreen__message"> 
+                  
+                    <p className="chatScreen__text">{m.text}</p>
+                </div>) 
+            : 
+            ( 
+                <div className="chatScreen__message"> 
+                    <p className="chatScreen__textUser">{m.text}</p>
+                </div>
+            )
+          
+        ))}
+        </div>
+        <form onSubmit={handleSubmit} className="chatScreen__input">
+            <input 
+                className="chatScreen__inputField" 
+                value={messageContent}  
+                onChange={(event) => setMessageContent(event.target.value)}
+                type="text" 
+                placeholder="Type a message"
+            />
+            <button type='submit' className="chatScreen__inputButton">SEND</button>
+        </form>
     </div>
   );
 }
